abeb6427809ccc5205de876ba0353b04d3b5ab56
TheRealSS <sharad97@gmail.com>
2020-11-01 23:16:17 +0530

    Finish about section prototype

--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import './index.css';
 import 'bulma/css/bulma.min.css';
 
 import Navbar from './components/Navbar'
+import About from './layouts/About'
+import Menu from './components/Menu'
 
 const App = () => {
   return (
-    <div className="container">
+    <div className="container p-6">
       <Navbar />
+
+      <div className="columns">
+        <div className="column is-one-quarter">
+          <Menu />
+        </div>
+        <div className="column">
+          <About />
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/layouts/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import 'bulma/css/bulma.min.css'
+
+const About = () => {
+
+  return (
+    <div className="container">
+      <h1 class="title">
+        About Me     
+      </h1>
+      <p>
+        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+        Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. 
+        Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. 
+        Mauris placerat eleifend leo.
+      </p>
+    </div>
+  )
+}
+
+export default About
