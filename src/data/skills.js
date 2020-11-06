534590fa744554f08347346f4801e058449e3503
TheRealSS <sharad97@gmail.com>
2020-11-06 22:39:21 +0530

    Minor edits

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,21 +1,21 @@
 const lang = [
-    { name: 'Java', icon: 'devicon-java-plain'},
-    { name: 'Python', icon: 'devicon-python-plain'},
-    { name: 'JavaScript', icon: 'devicon-javascript-plain'},
-    { name: 'HTML5', icon: 'devicon-html5-plain'},
-    { name: 'CSS3', icon: 'devicon-css3-plain'},
+    { name: 'Java', icon: 'devicon-java-plain' },
+    { name: 'Python', icon: 'devicon-python-plain' },
+    { name: 'JavaScript', icon: 'devicon-javascript-plain' },
+    { name: 'HTML5', icon: 'devicon-html5-plain' },
+    { name: 'CSS3', icon: 'devicon-css3-plain' },
 ]
 
 const libs = [
-    { name: 'React', icon: 'devicon-react-original'},
-    { name: 'Express', icon: 'devicon-express-original'},
-    { name: 'Node.js', icon: 'devicon-nodejs-plain'},
+    { name: 'React', icon: 'devicon-react-original' },
+    { name: 'Express', icon: 'devicon-express-original' },
+    { name: 'Node.js', icon: 'devicon-nodejs-plain' },
 ]
 
 const tools = [
-    { name: 'Git', icon: 'devicon-git-plain'},
-    { name: 'Linux Shell', icon: 'devicon-linux-plain'},
-    { name: 'Heroku', icon: 'devicon-heroku-plain'},
+    { name: 'Git', icon: 'devicon-git-plain' },
+    { name: 'Linux Shell', icon: 'devicon-linux-plain' },
+    { name: 'Heroku', icon: 'devicon-heroku-plain' },
 ]
 
 const skills = { lang, libs, tools }
