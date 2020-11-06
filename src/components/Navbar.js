47380359ad600c7e0539660962a2fc3b8e8131fe
TheRealSS <sharad97@gmail.com>
2020-11-07 02:48:44 +0530

    Add navbar vars

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,11 @@ import logo from '../assets/logo.png'
 const Navbar = () => {
  
   return (
-    <nav className="navbar is-fixed-top">
+    <nav className="navbar is-transparent is-fixed-top" id="#top">
       
       <div className="navbar-brand">
-        <a href="/" className="navbar-item">
-          <img src={logo} />
+        <a href="#top" className="navbar-item">
+          <img src={logo} alt="" />
         </a>
       </div>
 
