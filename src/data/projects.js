c871af775a6f7c6f70ae0fcb6737212f7de8361e
TheRealSS <sharad97@gmail.com>
2020-11-08 22:46:11 +0530

    Modify project data

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ const projects = [
         name: 'det0x - Toxic Comments Classifier',
         desc: 'A web-app written in Python designed to classify toxic user comments on Twitter.',
         tags: ['Keras', 'Flask', 'Twitter API', 'Jinja', 'Python'],
-        link: '/',
+        link: 'https://gitlab.com/sharadsw/toxic-comments',
     },
     {
         name: 'Portfolio',
